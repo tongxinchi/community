3bc364e04cf8121e8310ed9dbaa85d79977e0d75
Administrator <805997293@qq.com>
2020-02-12 13:37:32 +0800

    test

---
+++ src/test/java/com/txc/pro1blog/Pro1blogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.txc.pro1blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro1blogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
